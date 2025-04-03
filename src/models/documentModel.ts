14522aadd0a39f84b055ce8cd3426a2b6534c713
Prachi Maheta <158591163+prachimaheta@users.noreply.github.com>
2025-04-03 09:58:47 +0530

    Delete src directory

--- src/models/documentModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const documentSchema = new mongoose.Schema({
-    _id: String,
-    name: String,
-    data: Object
-});
-
-export const Document = mongoose.model("document", documentSchema) ;
